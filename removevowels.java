12a3ede0a914592b65a5b47e2a10c1a85a7bfd0d
Avani Jain <52358306+avani5jain@users.noreply.github.com>
2019-08-11 16:31:03 +0530

    Create removevowels.java

---
+++ removevowels.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String str=sc.next();
+	
+		String str1=str.replaceAll("[aeiouAEIUO]","");
+		System.out.print(str1);
+
+	}
+}
